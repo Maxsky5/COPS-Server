029339c28f33269e784de23b0614d88da6cbd3b4
Maxime <ribera.maxime@gmail.com>
2015-11-04 14:37:54 +0100

    Initial commit

---
+++ src/main/java/com/cesi/CopsServerApplication.java
@@ -0,0 +1,12 @@
+package com.cesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopsServerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cesi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cesi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CopsServerApplication.class);
+	}
+
+}
